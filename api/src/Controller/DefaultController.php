9d48ca9c269d5b9eebeb01defd5f943b32ea2b61
ginokok1996 <ginokok1996@hotmail.com>
2020-12-14 14:24:51 +0100

    first update

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -28,43 +28,7 @@ class DefaultController extends AbstractController
      * @Route("/")
      * @Template
      */
-    public function indexAction(CommonGroundService $commonGroundService, MailingService $mailingService, Request $request, ParameterBagInterface $params)
-    {
-        // On an index route we might want to filter based on user input
-        $variables['query'] = array_merge($request->query->all(), $variables['post'] = $request->request->all());
-
-        if ($this->getUser()) {
-            $person = $commonGroundService->getResource($this->getUser()->getPerson());
-            $personUrl = $commonGroundService->cleanUrl(['component' => 'cc', 'type' => 'people', 'id' => $person['id']]);
-
-            $employees = $commonGroundService->getResourceList(['component' => 'mrc', 'type' => 'employees'], ['person' => $personUrl])['hydra:member'];
-
-            if (!count($employees) > 0) {
-                $mailingService->sendMail('mails/welcome_mail.html.twig', 'no-reply@conduction.academy', $this->getUser()->getUsername(), 'Welkom op conduction.academy');
-
-                $employee = [];
-                $employee['person'] = $personUrl;
-
-                $commonGroundService->createResource($employee, ['component' => 'mrc', 'type' => 'employees']);
-
-                $providers = $commonGroundService->getResourceList(['component' => 'uc', 'type' => 'providers'], ['type' => 'id-vault', 'application' => $params->get('app_id')])['hydra:member'];
-                $provider = $providers[0];
-
-                $users = $commonGroundService->getResourceList(['component' => 'uc', 'type' => 'users'], ['username' => $this->getUser()->getUsername()])['hydra:member'];
-                $user = $users[0];
-
-                $userUrl = $commonGroundService->cleanUrl(['component' => 'uc', 'type' => 'users', 'id' => $user['id']]);
-            }
-        }
-
-        return $variables;
-    }
-
-    /**
-     * @Route("/login")
-     * @Template
-     */
-    public function loginAction(CommonGroundService $commonGroundService, Request $request)
+    public function indexAction(Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, string $slug = 'home')
     {
         $variables = [];
 
@@ -72,59 +36,16 @@ class DefaultController extends AbstractController
     }
 
     /**
-     * @Route("/register")
+     * @Route("/ambtenaar")
      * @Template
      */
-    public function registerAction(CommonGroundService $commonGroundService, Request $request)
+    public function ambtenaarAction(Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, string $slug = 'home')
     {
         $variables = [];
 
-        return $variables;
-    }
+        $variables['products'] = $commonGroundService->getResourceList(['component' => 'pdc', 'type' => 'products'], ['groups.id' => '7f4ff7ae-ed1b-45c9-9a73-3ed06a36b9cc'])['hydra:member'];
 
-    /**
-     * @Route("/organization")
-     * @Template
-     */
-    public function organizationAction(CommonGroundService $commonGroundService, Request $request)
-    {
-        $variables = [];
-
-        if (!$this->getUser()) {
-            return $this->redirect($this->generateUrl('app_user_idvault').'?backUrl='.$request->getUri());
-        }
-
-        if ($request->query->get('backUrl')) {
-            $variables['backUrl'] = $request->query->get('backUrl');
-        }
 
         return $variables;
-    }
-
-    /**
-     * @Route("/newsletter")
-     * @Template
-     */
-    public function newsletterAction(Session $session, Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params, EventDispatcherInterface $dispatcher)
-    {
-        // TODO: use email used in form to subscribe to the newsletter?
-
-        $session->set('backUrl', $request->query->get('backUrl'));
-
-        $providers = $commonGroundService->getResourceList(['component' => 'uc', 'type' => 'providers'], ['type' => 'id-vault', 'application' => $params->get('app_id')])['hydra:member'];
-        $provider = $providers[0];
-
-        $redirect = $this->generateUrl('app_default_index', ['message' => 'you have successfully signed up for the newsletter!'], UrlGeneratorInterface::ABSOLUTE_URL);
-
-        if (isset($provider['configuration']['app_id']) && isset($provider['configuration']['secret'])) {
-            $dev = '';
-            if ($params->get('app_env') == 'dev') {
-                $dev = 'dev.';
-            }
-
-            return $this->redirect('http://id-vault.com/sendlist/authorize?client_id='.$provider['configuration']['app_id'].'&send_lists=8b929e53-1e16-4e59-a254-6af6b550bd08&redirect_uri='.$redirect);
-        } else {
-            return $this->render('500.html.twig');
-        }
     }
 }
